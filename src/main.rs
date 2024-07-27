9f55f8471e80e43535791ae1870e4a57fe28d1b1
qti3e <i@parsa.ooo>
2024-07-27 19:24:07 -0400

    lifetimes and borrow checker demo

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,26 @@
-struct FibIter {
-    prev: u64,
-    next: u64,
-}
-
-impl FibIter {
-    pub fn new() -> Self {
-        // 1 1 2 3 5
-        FibIter { prev: 0, next: 1 }
-    }
-}
-
-impl Iterator for FibIter {
-    type Item = u64;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        let next = self.prev + self.next;
-        self.prev = self.next;
-        self.next = next;
-        Some(self.prev)
-    }
-}
-
 fn main() {
-    for (i, n) in FibIter::new().take(5).enumerate() {
-        println!("Fib({}) = {n}", i + 1);
+    let mut list1 = vec![1, 2];
+    let list2 = vec![3, 4, 5];
+
+    let mut other: Vec<&i32> = vec![];
+
+    append_by_ref(&mut other, &list1);
+    append_by_ref(&mut other, &list2);
+    append_by_ref(&mut other, &list1);
+
+    dbg!(&other);
+    dbg!(&list1);
+
+    // list1.push(100);
+    // ^-- this will not work!
+
+    drop(other);
+
+    list1.push(100);
+}
+
+fn append_by_ref<'a, 'b, T>(target: &'a mut Vec<&'b T>, source: &'b Vec<T>) {
+    for x in source.iter() {
+        target.push(x);
     }
-    println!("exited...");
 }
