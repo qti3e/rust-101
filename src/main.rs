f18f750f6260979b4db7d4bb7a44b7afd6705278
qti3e <i@parsa.ooo>
2024-07-27 19:25:32 -0400

    VecIter: lifetime iter

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,31 @@
-fn main() {
-    let mut list1 = vec![1, 2];
-    let list2 = vec![3, 4, 5];
-
-    let mut other: Vec<&i32> = vec![];
-
-    append_by_ref(&mut other, &list1);
-    append_by_ref(&mut other, &list2);
-    append_by_ref(&mut other, &list1);
-
-    dbg!(&other);
-    dbg!(&list1);
-
-    // list1.push(100);
-    // ^-- this will not work!
-
-    drop(other);
-
-    list1.push(100);
+struct VecIter<'a, T> {
+    v: &'a Vec<T>,
+    index: usize,
 }
 
-fn append_by_ref<'a, 'b, T>(target: &'a mut Vec<&'b T>, source: &'b Vec<T>) {
-    for x in source.iter() {
-        target.push(x);
+impl<'a, T> VecIter<'a, T> {
+    pub fn new<'b: 'a>(v: &'b Vec<T>) -> Self {
+        Self { v, index: 0 }
+    }
+}
+
+impl<'a, T> Iterator for VecIter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.index < self.v.len() {
+            let n = self.index;
+            self.index += 1;
+            Some(&self.v[n])
+        } else {
+            None
+        }
+    }
+}
+
+fn main() {
+    let numbers = vec![2, 7, 11, 15];
+    for n in VecIter::new(&numbers) {
+        println!("element: {n}");
     }
 }
