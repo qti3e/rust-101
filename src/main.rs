793c7e7f81f41cb5fc140ab3ead8e297e5ad5329
qti3e <i@parsa.ooo>
2024-07-31 17:08:08 -0400

    VecIterMut: slice is better

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,44 @@
-struct VecIter<'a, T> {
-    v: &'a Vec<T>,
-    index: usize,
+// owning: T
+// mutable borrow: &mut T
+// borrow: &T
+
+pub struct VecIterMut<'a, T: 'a> {
+    // &'mut [T], &'mut [T...]
+    slice: &'a mut [T],
 }
 
-impl<'a, T> VecIter<'a, T> {
-    pub fn new<'b: 'a>(v: &'b Vec<T>) -> Self {
-        Self { v, index: 0 }
-    }
-}
-
-impl<'a, T> Iterator for VecIter<'a, T> {
-    type Item = &'a T;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.index < self.v.len() {
-            let n = self.index;
-            self.index += 1;
-            Some(&self.v[n])
-        } else {
-            None
+impl<'a, T: 'a> VecIterMut<'a, T> {
+    pub fn new(vec: &'a mut Vec<T>) -> Self {
+        Self {
+            slice: vec.as_mut_slice(),
         }
     }
 }
 
-fn main() {
-    let numbers = vec![2, 7, 11, 15];
-    for n in VecIter::new(&numbers) {
-        println!("element: {n}");
+impl<'a, T: 'a> Iterator for VecIterMut<'a, T>
+where
+    T: std::fmt::Debug,
+{
+    type Item = &'a mut T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.slice.is_empty() {
+            return None;
+        }
+
+        let old_slice = std::mem::replace(&mut self.slice, &mut []);
+        let (left, right) = old_slice.split_at_mut(1);
+
+        self.slice = right;
+
+        Some(&mut left[0])
     }
+}
+
+fn main() {
+    let mut vec = vec![2, 7, 11, 15];
+    for x in VecIterMut::new(&mut vec) {
+        *x += 1;
+    }
+    dbg!(vec);
 }
