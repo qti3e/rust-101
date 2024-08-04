a1aa51fd0677e9d64791741e38b9d0610d0c723d
qti3e <i@parsa.ooo>
2024-08-04 19:39:52 -0400

    thread sync with mpsc

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,37 @@
-use rand::rngs::OsRng;
-use rand::{thread_rng, Rng};
-
-fn x<R: Rng>(rng: &mut R) -> (u32, u32) {
-    (rng.gen(), rng.gen())
-}
+use std::sync::mpsc;
 
 fn main() {
-    dbg!(x(&mut OsRng));
-    dbg!(x(&mut thread_rng()));
+    let (tx1, rx1) = mpsc::channel::<()>();
+    let (tx2, rx2) = mpsc::channel::<()>();
+
+    let h1 = std::thread::spawn(move || {
+        for i in 0..100 {
+            // ->h2: hey print something.
+            // -> wait for h2's command.
+
+            tx2.send(()).unwrap();
+            rx1.recv().unwrap();
+
+            println!("A {i}");
+        }
+    });
+
+    let h2 = std::thread::spawn(move || {
+        for i in 0..100 {
+            // -> wait for h1's command.
+            rx2.recv().unwrap();
+            println!("B {i}");
+            tx1.send(()).unwrap();
+            // ->h1: print something
+        }
+    });
+
+    // ...
+    // for i in 0..100 {
+    //     println!("Z {i}");
+    // }
+
+    h1.join().unwrap();
+    h2.join().unwrap();
 }
+
