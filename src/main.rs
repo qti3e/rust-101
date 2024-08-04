3809ec7a185ab7477c12b33b668ca78b7e79c596
qti3e <i@parsa.ooo>
2024-08-04 19:09:17 -0400

    demo: using a crate (rand)

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,11 @@
-// owning: T
-// mutable borrow: &mut T
-// borrow: &T
+use rand::rngs::OsRng;
+use rand::{thread_rng, Rng};
 
-pub struct VecIterMut<'a, T: 'a> {
-    index: usize,
-    vec: &'a mut Vec<T>,
-}
-
-impl<'a, T: 'a> VecIterMut<'a, T> {
-    pub fn new(vec: &'a mut Vec<T>) -> Self {
-        Self { index: 0, vec }
-    }
-}
-
-impl<'a, T: 'a> Iterator for VecIterMut<'a, T>
-where
-    T: std::fmt::Debug,
-{
-    type Item = &'a mut T;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.index >= self.vec.len() {
-            return None;
-        }
-
-        let index = self.index;
-        self.index += 1;
-        // why does this not work?
-        // imagine the prev line was commented out.
-        Some(&mut self.vec[index]) // &mut 2
-    }
+fn x<R: Rng>(rng: &mut R) -> (u32, u32) {
+    (rng.gen(), rng.gen())
 }
 
 fn main() {
-    let mut vec = vec![2, 7, 11, 15];
-    let mut iter = VecIterMut::new(&mut vec);
-    let x1 = iter.next().unwrap();
-    let x2 = iter.next().unwrap();
-    dbg!(x1, x2); // &mut 2, &mut 2 // -> now we have 2 mutable access to the same value
-                  // this violates the borrow checker.
-
-    for x in VecIterMut::new(&mut vec) {
-        // x == &mut 2
-        // x == &mut 2
-        *x += 1;
-    }
-
-    dbg!(vec);
+    dbg!(x(&mut OsRng));
+    dbg!(x(&mut thread_rng()));
 }
